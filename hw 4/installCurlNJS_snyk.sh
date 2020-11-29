da6f0072f2b1ff650322f4992bedbf5ff4c60e29
PtatG <55119325+PtatG@users.noreply.github.com>
2020-11-29 05:06:59 -0500

    Add files via upload

---
+++ hw 4/installCurlNJS_snyk.sh
@@ -0,0 +1,15 @@
+# mvn housekeeping
+source /etc/profile.d/maven.sh
+#Install curl & NodeJS
+sudo apt install curl |tee -a install.log
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash - |tee -a install.log
+sudo apt-get install -y nodejs |tee -a install.log
+
+sudo npm update |tee -a install.log
+sudo npm i -g snyk |tee -a install.log
+sudo chown $USER -R ~/.config |tee -a install.log
+
+echo "Sign in to your GitHub account"
+echo "When complete then enter the command:"
+echo "snyk auth"
+echo "This will complete the nodeJS and snyk installation."
